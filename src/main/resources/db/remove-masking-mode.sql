dabfcb3e2a82ae7c0177738870472984ef3a3c29
TabFor <2244530215@qq.com>
2025-04-22 16:41:08 +0800

    删除不再使用的文件和配置，包括文档、数据库脚本、类文件及相关资源，清理项目结构以提高可维护性。

--- src/main/resources/db/remove-masking-mode.sql
+++
@@ -1,2 +0,0 @@
--- 从masking_rules表中移除masking_mode字段
-ALTER TABLE masking_rules DROP COLUMN masking_mode; 
--- src/main/resources/db/update-masking-rules.sql
+++
@@ -1,2 +0,0 @@
--- 在masking_rules表中添加masking_mode字段
-ALTER TABLE masking_rules ADD COLUMN masking_mode VARCHAR(50) NOT NULL DEFAULT 'BUILT_IN'; 
